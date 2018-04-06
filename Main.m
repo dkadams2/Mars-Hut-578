fb5a3aaa10a246a9c12a834b436bb9f6802d2676
dkadams2 <derrik_adams3@hotmail.com>
2018-04-05 20:12:33 -0600

    Changed 'Main' file to the DOE one and just call cal_main

--- Main.m
+++ Main.m
@@ -1,63 +1,28 @@
-%ME 578 Project Main 
+%ME 578 - Mars Hut Project
 %Derrik Adams
-%4/3/18
-%ALL UNITS ARE IN METERS
-% clear all;
-% clc;
-function [Habitat_material_volume] = Main(people, duration, lab_equip)
-%Get number of people and mission duration. This will come from JMP GUI
-%input. Mission duration will be time on Mars not including the travel time
-%to get there.
-% people = input('Number of People: ');
-% duration = input('Mission Duration (weeks): ');
-% lab_equip = input('Number of Lab Equipment: ');
-wall_thickness = .05;
+%4/5/18
+%Main script to take in user input from JMP and call cal_main to compute
+%the total material needed for the entire habitat, the total cost of
+%acquiring and shipping it, and to obtain the number and dimensions of each
+%type of dome in the structure
+clear all
+% DOE = dlmread('Habitat_DOE.txt');
+% people = DOE(:,1);
+% duration = DOE(:,2);
+% lab_equip = DOE(:,3);
+% for i = 1:length(people)
+%     [Habitat_material_volume(i), Total_cost(i), Num_Farm_Domes, Num_Lab_Domes, Num_Storage_Domes,... 
+%     Radius_Farm, Radius_Lab, Radius_Storage, Tube_Length, Tube_Radius, Barracks_Length, Barracks_Width, Barracks_Height] = cal_main(people(i),duration(i),lab_equip(i));
+% end
+% x = 1:1:i;
+% Habitat_material_volume = Habitat_material_volume';
+% Total_cost = Total_cost';
+% plot(x,Habitat_material);
 
-%Based on input, calculate the volume per person needed using JMP model
-vol_pp = 3+.2*duration;
-
-%Calculate the Required LAB AREA (m^2)
-lab_area = cal_lab_size(people, lab_equip);
-
-%Calculate the required FARM AREA. If mission is less than 6 months, then a
-%farm is unnecessary (m^2)
-if duration > 24
-    farm_area = people*62;
-else
-    farm_area = 0;
-end
-
-%Calculate initial/total food supply
-vol_per_meal = .3*.15*.03;
-vol_per_day_pp = 3*vol_per_meal;
-vol_food4trip = vol_per_day_pp*people*270; %Trip is approximately 9 months
-
-if duration > 24
-   vol_food_mars = vol_per_day_pp*people*(24*7); %This is a 6-month supply of food assuming they can grow their own
-   vol_food = vol_food_mars+vol_food4trip;
-else
-    vol_food_mars = vol_per_day_pp*people*duration*7; %Take all the food they need if not growing any
-    vol_food = vol_food_mars+vol_food4trip;
-end
-
-%Call function to calculate the needed volume of material to build the
-%farm, lab, and storage dome along with the connecting tubes
-[opt_num_farm_domes, opt_num_lab_domes, opt_num_storage_domes, farm_rad, lab_rad, storage_rad, tube_length, tube_rad, fls_material_vol] = cal_habitat_size(farm_area, lab_area, wall_thickness, vol_food);
+people = input('Number of People: ');
+duration = input('Mission Duration (weeks): ');
+lab_equip = input('Number of Lab Equipment: ');
 
 
-%Call MIDACO to optimize the living space
-executable = 'Mars_Habitat_Optimizer.exe %d %d %d';
-exe_run = sprintf(executable, people, wall_thickness, vol_pp);
-habitat_info = system(exe_run);
-
-%Read in the data from the optimization run
-barracks_data = dlmread('Optimized_Habitat.txt');
-barracks_length = barracks_data(1);
-barracks_width = barracks_data(2);
-barracks_height = barracks_data(3);
-barracks_material_vol = barracks_data(4);
-
-%Calculate the total volume of habitat material needed for the entire
-%structure
-Habitat_material_volume = barracks_material_vol + fls_material_vol;
-end
+[Habitat_material_volume, Total_cost, Num_Farm_Domes, Num_Lab_Domes, Num_Storage_Domes,... 
+    Radius_Farm, Radius_Lab, Radius_Storage, Tube_Length, Tube_Radius, Barracks_Length, Barracks_Width, Barracks_Height] = cal_main(people,duration,lab_equip);
--- cal_total_cost.m
+++ cal_total_cost.m
@@ -4,22 +4,36 @@
 
 %Look for cost estimating relationships
 
-function [total_cost] = cal_total_cost(num_people, material_volume, food_mass, equipment_volume)
+function [total_cost] = cal_total_cost(num_people, material_volume,vol_food_pm, vol_food, equipment_area)
 
-%Define cost of materials
+%Define cost of HABITAT MATERIALS
 habitat_material_density = 4.43; %kg/m^3 (Ti-6Al-4V)
 habitat_material_cost = 50; %$/kg (approximate)
-food_cost = 20; %$/kg (total guess)
-equipment_cost = 100; %$/m^3 (total guess)
+
+%Define cost of FOOD
+num_meals = vol_food/vol_food_pm;
+food_mass = num_meals*.83; %According to paper, 1 meal is .83kg (with packaging included)
+food_cost_per_kg = 10; %$/kg (total guess)
+
+%Define cost of LAB EQUIPMENT
+equipment_cost = 1000; %$/m^2 (total guess)
+equipment_unit_mass = 100; %kg/m^2
+equipment_mass = equipment_unit_mass*equipment_area;
+
+%Define human average mass for rocket costs
 human_mass = 75; %kg
-space_cost = 22000; %$/kg to send something to space
+
+%Average cost to send 1kg of mass into space
+space_cost = 22000; %$/kg 
 
 %Calculate just raw material cost
 total_habitat_material_cost = material_volume * habitat_material_density * habitat_material_cost;
-total_food_cost = food_mass * food_cost;
-total_equipment_cost = equipment_volume * equipment_cost;
+total_food_cost = food_mass * food_cost_per_kg;
+total_equipment_cost = equipment_area * equipment_cost;
 
 %Calculate the total mass of stuff being sent to get "rocket cost"
 total_mass = (material_volume*habitat_material_density) + food_mass + equipment_mass + (num_people*human_mass);
 
-total_cost = total_mass*space_cost; 
+total_rocket_cost = total_mass*space_cost; 
+
+total_cost = total_rocket_cost + total_habitat_material_cost+total_food_cost+total_equipment_cost;
